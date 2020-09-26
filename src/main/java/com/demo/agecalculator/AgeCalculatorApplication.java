069469f3a093496e643e694008755591f0dba087
Vaibhav Deore <vaibhavdeore5@gmail.com>
2020-09-26 17:37:30 +0530

    second commit

--- src/main/java/com/demo/agecalculator/AgeCalculatorApplication.java
+++ src/main/java/com/demo/agecalculator/AgeCalculatorApplication.java
@@ -2,8 +2,10 @@ package com.demo.agecalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.demo")
 public class AgeCalculatorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/demo/controller/AgeCalculator.java
+++ src/main/java/com/demo/controller/AgeCalculator.java
@@ -1,33 +1,30 @@
 package com.demo.controller;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.Period;
-
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AgeCalculator {
 
-
-	@RequestMapping(value = "/getAge", method = RequestMethod.GET)
-	
-	public String getAge(@PathVariable("date") String date) {
+	@RequestMapping(value = "/getAge", method = RequestMethod.GET)		
+	public String getAge(@RequestParam(name="date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) 
+	{
 		
-		LocalDate today = LocalDate.now();                          //Today's date
-		LocalDate birthday = LocalDate.of(1990, Month.AUGUST, 14);  //Birth date
-		 
-		Period p = Period.between(birthday, today);
-		 
+		LocalDate today = LocalDate.now();                          
+		LocalDate birthday = LocalDate.of(date.getYear(),date.getMonth(), date.getDayOfMonth());  
+		
+		Period p = Period.between(birthday, today);		 
 		//Now access the values as below
 		System.out.println(p.getDays());
 		System.out.println(p.getMonths());
 		System.out.println(p.getYears());
 		
-		return String.valueOf(p.getYears());
+		return String.valueOf(p.getYears() + " Years" + " " + p.getMonths() + " Months" +" " +p.getDays() +" " + " Days");
 	}
-	
 }
